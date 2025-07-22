5dfa1d39aa29ae6b556b42b981abffba009f2784
gravblue <110542371+gravblue@users.noreply.github.com>
2025-07-23 00:04:10 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
+
 export default function App() {
-  useEffect(() => {
-    const interval = setInterval(() => {
-      fetch("https://nandaputric-suararasa.hf.space/");
-    }, 30000); // ping setiap 30 detik
-
-    return () => clearInterval(interval); // bersihkan interval saat unmount
-  }, []);
-
-  return (
-    <div className="min-h-screen bg-black text-white">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </div>
-  );
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
